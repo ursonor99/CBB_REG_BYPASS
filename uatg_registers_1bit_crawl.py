759b63782b45ca7751f7d983205e603bfefe8492
Rathish A T <52832396+ursonor99@users.noreply.github.com>
2021-12-31 16:39:16 +0530

    Update uatg_registers_1bit_crawl.py

--- uatg_registers_1bit_crawl.py
+++ uatg_registers_1bit_crawl.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Union, Any
 import random
 
 
-class uatg_rathish_rf01(IPlugin):
+class uatg_registers_1bit_crawl(IPlugin):
     """
     This class contains methods to generate and validate the tests such that
     1. No bit in RTL is stuck at 0
