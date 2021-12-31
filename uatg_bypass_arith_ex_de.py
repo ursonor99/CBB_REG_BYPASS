b3dadb67a9303414bc2fa30023edf7d29288e75f
Rathish A T <52832396+ursonor99@users.noreply.github.com>
2021-12-31 16:37:49 +0530

    Update uatg_bypass_arith_ex_de.py

--- uatg_bypass_arith_ex_de.py
+++ uatg_bypass_arith_ex_de.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Union, Any
 import random
 
 
-class uatg_rathish_bp01(IPlugin):
+class uatg_bypass_arith_ex_de(IPlugin):
     """
     This class contains methods to generate and validate the tests such that
     if the value of register after multiple operations remains unchanged.
